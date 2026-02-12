08b9295dbb453410380f9cdf08fee062e2c35c78
Charles <thornct3@gmail.com>
2026-02-12 11:10:32 -0500

    cors update

--- backend/api.py
+++ backend/api.py
@@ -6,11 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "http://50.116.38.143:3000",
-        "http://50.116.38.143"
-    ],
+    allow_origins=["*"],  # Allow all origins for development
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
